e47971920d447d380e6c4b73ff24bc696a7a9fc4
minerva-jupiter <ryouturn@gmail.com>
2025-11-21 07:30:59 +0900

    feat(app): Set up core application UI, navigation, and configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Fun Fiction Storage",
@@ -17,21 +18,23 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body>
-        <header>
+      <body className="flex flex-col min-h-screen">
+        <header className="bg-gray-800 text-white p-4">
           <nav>
-            <ul>
+            <ul className="flex space-x-4">
               <li>
-                <a href="/">Home</a>
+                <Link href="/">Home</Link>
               </li>
               <li>
-                <a href="about">About</a>
+                <Link href="/about">About</Link>
               </li>
             </ul>
           </nav>
         </header>
-        <main>{children}</main>
-        <footer>@ all rights reserved.</footer>
+        <main className="grow p-4">{children}</main>
+        <footer className="bg-gray-800 text-white p-4 text-center">
+          @ all rights reserved.
+        </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 export default function Home() {
   return (
-    <main>
-      <div className="container">
-        <h1>Welcome to Fun Fiction Storage</h1>
-        <p>Explore our collection of stories and share your own.</p>
-      </div>
+    <main className="container">
+      <h1 className="text-4xl font-bold mb-4">
+        Welcome to Fun Fiction Storage
+      </h1>
+      <p className="text-lg text-gray-700">
+        Explore our collection of stories and share your own.
+      </p>
+      <br />
+      <h2 className="text-3xl font-bold mb-2">一覧</h2>
     </main>
   );
 }
